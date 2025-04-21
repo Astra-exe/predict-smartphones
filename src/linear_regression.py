eea7204981b1affcc1a821ee3e603f074b519046
Astra-exe <juan.ramirez.j99@gmail.com>
2025-04-20 23:44:25 -0600

    [Add] Linear regression algorithm from scratch

---
+++ src/linear_regression.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, learnging_rate=0.01, epochs=1000):
+        self.lr = learnging_rate
+        self.epochs = epochs
+        self.theta = None #Model weights
+        self.loss_history = []
+    
+    def fit(self, X, y):
+        #train the model with gradient descent
+        m,n = X.shape
+        self.theta = np.zeros((n,1))
+
+        for epoch in range(self.epochs):
+            #predictions and error
+            y_pred = X.dot(self.theta)
+            error = y_pred - y
+
+            #cost (MSE)
+            cost = (1/(2*m)) * np.sum(error**2)
+            self.loss_history.append(cost)
+
+            #calculate gradients
+            gradients = (1/m) * X.T.dot(error)
+
+            #update weights
+            self.theta -= self.lr * gradients
+        return self
+    
+    def predict(self, X):
+        #make predictions
+        return X.dot(self.theta)
+    
+    #Functions for metrics
+    def mse(self, y_true, y_pred):
+        #calculate the mean square error
+        return np.mean((y_true-y_pred)**2)
+    
+    def r2_score(self, y_true, y_pred):
+        #calculate the r^2 score
+        ss_total = np.sum((y_true - np.mean(y_true))**2)
+        ss_res = np.sum((y_true-y_pred)**2)
+        return 1 - (ss_res/ss_total)
